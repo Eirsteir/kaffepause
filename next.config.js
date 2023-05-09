808fd12c8f98eb687c162e43be3457c72da85344
Eirik Steira <steiraeirik@gmail.com>
2023-05-09 17:54:05 +0200

    Fix eslint config error

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // To prevent slow dev build time
-  // experimental: {
-  //   modularizeImports: {
-  //     '@mui/material/?(((\\w*)?/?)*)': {
-  //       transform: '@mui/material/{{ matches.[1] }}/{{member}}'
-  //     },
-  //     '@mui/icons-material/?(((\\w*)?/?)*)': {
-  //       transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}'
-  //     }
-  //   }
-  // },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreBuildErrors: true,
+    ignoreDuringBuilds: true,
   },
   swcMinify: true,
 };
