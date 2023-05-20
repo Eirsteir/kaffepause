7c6d97ce1969271da78d9da3f464eadee9a57782
Eirik Steira <steiraeirik@gmail.com>
2023-05-20 14:26:32 +0200

    Disable cors for apollo client

--- src/apollo-client-setup.tsx
+++ src/apollo-client-setup.tsx
@@ -12,6 +12,9 @@ import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_API_URL,
+  fetchOptions: {
+    mode: 'no-cors',
+  },
 });
 
 const authLink = setContext(async (_, { headers }) => {
