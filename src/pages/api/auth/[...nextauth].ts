69e69c82ef10aa7c04e9a24cfc82e04640035137
Eirik Steira <steiraeirik@gmail.com>
2023-05-23 18:45:08 +0200

    Add neo4j driver dependencies

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,84 +1,20 @@
-import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import neo4j from 'neo4j-driver';
+import NextAuth from 'next-auth/next';
 
-import apolloClient from '@/apollo-client-setup';
-import ME_QUERY from '@/graphql/me.query';
-import SIGNIN_MUTATION from '@/graphql/signin.mutation';
-import URLS from '@/URLS';
+import { Neo4jAdapter } from '@next-auth/neo4j-adapter';
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      id: 'email-login',
-      name: 'Email',
-      credentials: {
-        email: { label: 'Email', type: 'text', placeholder: 'jsmith@test.pl' },
-        password: { label: 'Password', type: 'password' },
-      },
-      session: {
-        jwt: true,
-        maxAge: 30 * 24 * 60 * 60,
-      },
-      secret: process.env.NEXTAUTH_SECRET,
-      async authorize(credentials) {
-        const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
+const driver = neo4j.driver(
+  // neo4j://neo4j:kaffepauseadmin@neo4j:7687
+  'bolt://localhost',
+  neo4j.auth.basic('neo4j', 'kaffepauseadmin'),
+);
 
-        const { data } = await apolloClient
-          .mutate({
-            mutation: SIGNIN_MUTATION,
-            variables: {
-              email: email,
-              password: password,
-            },
-          })
-          .catch((error) => {
-            console.error('Something went wrong during login mutation', error);
-            throw new Error(
-              'Noe gikk galt da vi prøvde å logge deg inn. Prøv igjen senere.',
-            );
-          });
+const neo4jSession = driver.session();
 
-        console.log('data', data);
-        const { tokenAuth } = data;
-
-        if (tokenAuth.success) {
-          return {
-            access_token: tokenAuth.token,
-            refresh_token: tokenAuth.refreshToken,
-            user: tokenAuth.user,
-          };
-        } else {
-          console.log('tokenAuth', tokenAuth.errors.nonFieldErrors);
-          throw new Error('Feil epost eller passord. Prøv igjen.');
-        }
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        token.account = user;
-      }
-      return token;
-    },
-    async session({
-      session,
-      token: {
-        account: { user },
-      },
-    }) {
-      session.user = user;
-      return session;
-    },
-  },
-  pages: {
-    signIn: URLS.SIGNIN,
-    // error: URLS.SIGNIN
-  },
-};
-
-export default NextAuth(authOptions);
+// For more information on each option (and a full list of options) go to
+// https://authjs.dev/reference/configuration/auth-options
+export default NextAuth({
+  // https://authjs.dev/reference/providers/oauth-builtin
+  providers: [],
+  adapter: Neo4jAdapter(neo4jSession),
+});
