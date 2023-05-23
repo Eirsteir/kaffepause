4f31736c9502eeb050920220bd9a2ea8a46c0a32
Eirik Steira <steiraeirik@gmail.com>
2023-05-23 16:13:53 +0200

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,24 @@ import { ApolloProviderWrapper } from '@/apollo-client-setup';
 import Layout from '@/components/Layout';
 import { theme } from '@/theme';
 import { ThemeProvider } from '@mui/material/styles';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <ApolloProviderWrapper>
-      <SessionProvider session={session}>
-        <ThemeProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </SessionProvider>
-    </ApolloProviderWrapper>
+    <>
+      <ApolloProviderWrapper>
+        <SessionProvider session={session}>
+          <ThemeProvider theme={theme}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ThemeProvider>
+        </SessionProvider>
+      </ApolloProviderWrapper>
+      <Analytics />
+    </>
   );
 }
