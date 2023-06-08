1858b25c33fea85c68d9284a0699a0475af0403b
Eirik Steira <steiraeirik@gmail.com>
2023-06-08 17:52:44 +0200

    Responsive next break card

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,28 @@
 import PageContainer from '@/components/elements/PageContainer';
 import PendingBreakInvitations from '@/components/home/PendingInvitations';
 import BreakPlanner from '@/components/modules/planning/BreakPlanner';
+import NextBreakActionCard from '@/components/navigation/NextBreakActionCard';
 import { QueryResult } from '@/components/QueryResult';
 import { useMe } from '@/hooks/User';
+import { useMediaQuery } from '@mui/material';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
 
 export default function Home() {
   const { data, loading, error } = useMe();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'), {
+    noSsr: true,
+  });
 
   return (
     <PageContainer>
-      <QueryResult data={data?.me} error={error} loading={loading}>
+      {isMobile && (
+        <Box sx={{ flex: '1 1 0px' }}>
+          <NextBreakActionCard />
+        </Box>
+      )}
+      <QueryResult data={data} error={error} loading={loading}>
         <BreakPlanner user={data?.me} />
         <PendingBreakInvitations />
       </QueryResult>
--- src/components/navigation/AuthenticatedNav.tsx
+++ src/components/navigation/AuthenticatedNav.tsx
@@ -5,18 +5,27 @@ import AccountMenu from '@/components/navigation/AccountMenu';
 import BaseNavigation from '@/components/navigation/BaseNavigation';
 import NextBreakActionCard from '@/components/navigation/NextBreakActionCard';
 import NotificationsMenu from '@/components/navigation/NotificationsMenu';
+import { useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
 
 interface IProps {
   user: User;
 }
 
 export default function AuthenticatedNav({ user }: IProps) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'), {
+    noSsr: true,
+  });
+
   return (
     <BaseNavigation>
-      <Box sx={{ flex: '1 1 0px' }}>
-        <NextBreakActionCard />
-      </Box>
+      {!isMobile && (
+        <Box sx={{ flex: '1 1 0px' }}>
+          <NextBreakActionCard />
+        </Box>
+      )}
       <Box
         sx={{
           display: 'flex',
